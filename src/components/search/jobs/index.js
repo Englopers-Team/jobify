c5cf287087ea30e37a5e03a58d5262be21e07d27
Othabteh <o.thabteh@gmail.com>
2020-12-10 00:16:13 +0200

    job search

--- src/components/search/jobs/index.js
+++ src/components/search/jobs/index.js
@@ -1,5 +1,49 @@
+import React, { useState } from 'react';
+
+import superagent from 'superagent';
+import Form from 'react-bootstrap/Form';
+import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Results from './results';
+
+const jobsApi = 'https://jobify-app-v2.herokuapp.com/search/job';
+
 export default function SearchJobs() {
+  const [title, setTitle] = useState('');
+  const [location, setLocation] = useState('');
+  const [results, setResults] = useState([]);
+  const jobList = async (e) => {
+    e.preventDefault();
+
+    await superagent
+      .get(jobsApi)
+      .query({ title: title, location: location })
+
+      .then((data) => {
+        setResults([...data.body.resultDB, ...data.body.resultAPI]);
+      });
+  };
+  console.log(results);
+
   return (
-    <></>
-  )
+    <Container>
+      <Form onSubmit={jobList}>
+        <Row>
+          <Col>
+            <Form.Control required name='title' type='text' onChange={(e) => setTitle(e.target.value)} placeholder='Job Title' />
+          </Col>
+          <Col>
+            <Form.Control required name='location' type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Country' />
+          </Col>
+
+          <Button type='submit' variant='secondary'>
+            search
+          </Button>
+        </Row>
+        <Row>
+          <Results results={results} />
+        </Row>
+      </Form>
+    </Container>
+  );
 }
--- src/components/search/jobs/results.js
+++ src/components/search/jobs/results.js
@@ -1,5 +1,34 @@
-export default function JobsResults() {
+import Table from 'react-bootstrap/Table';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import Button from 'react-bootstrap/Button';
+export default function JobsResults(props) {
+  let results = props.results;
   return (
-    <></>
-  )
+    <>
+      <Container fluid>
+        <Row sm={10}>
+          <Col>Title</Col>
+          <Col>Company</Col>
+          <Col>location</Col>
+          <Col>Type</Col>
+          <Col></Col>
+        </Row>
+
+        {results.map((item) => {
+          return (
+            <Row sm={10}>
+              <Col>{item.title}</Col>
+              <Col>{item.company_name}</Col>
+              <Col>{item.location}</Col>
+              <Col>{item.type}</Col>
+              <Button onClick={console.log(item.email)} variant='praimary'>
+                Apply
+              </Button>
+            </Row>
+          );
+        })}
+      </Container>
+    </>
+  );
 }
