cbc3fd868575aafb6654eb564be9836206398846
messeili <mymoa.2014@gmail.com>
2020-12-10 17:51:11 +0200

    finish header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Routes from './routes'
-import AuthProvider from './context/auth'
-import SocketProvider from './context/socket'
-import Header from './components/header/'
-import Footer from './components/footer/'
+import Routes from './routes';
+import AuthProvider from './context/auth';
+import SocketProvider from './context/socket';
+import Header from './components/header/';
+import Footer from './components/footer/';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <AuthProvider>
       <SocketProvider>
-        <Header />
-        <Routes />
-        <Footer />
+        <BrowserRouter>
+          <Header />
+          <Routes />
+          <Footer />
+        </BrowserRouter>
       </SocketProvider>
     </AuthProvider>
   );
