d4ae0553df493e58ed253a4bf74edc72fe974b94
messeili <mymoa.2014@gmail.com>
2020-12-10 18:50:10 +0200

    first stage

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,18 @@
+import './styles.scss';
+import Button from 'react-bootstrap/Button';
+import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink, Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
 export default function Footer() {
   return (
-    <></>
-  )
+    <>
+      <Navbar fixed='bottom' className='footer'>
+        <Navbar.Brand href='#home'>Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text>© Jobify | 2020 All rights reserved.</Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
