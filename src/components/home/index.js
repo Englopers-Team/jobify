ba7807a2edd446af771d41116007ee6657974eb9
messeili <mymoa.2014@gmail.com>
2020-12-11 15:01:30 +0200

    ignore eslint cache

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,10 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <>
       test
       <Link to={{ pathname: '/signin' }}>test</Link>
+      <Link to={{ pathname: '/company/submitted-jobs' }}>company submitted jobs</Link>
     </>
-  )
+  );
 }
