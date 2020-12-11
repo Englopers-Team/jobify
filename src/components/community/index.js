d5907b2154668bd06a1f3feeaa05b04fbb470d54
AbdulhakimZatar <hakimzatar@gmail.com>
2020-12-11 18:06:03 +0200

    community homepage

--- src/components/community/index.js
+++ src/components/community/index.js
@@ -3,6 +3,8 @@ import { AuthContext } from '../../context/auth'
 import { Container, Row, Col, Card, Image, Form, Button, Alert } from 'react-bootstrap';
 import superagent from 'superagent'
 import './styles.scss'
+import { PlusCircle, ChatSquareTextFill, HeartFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom'
 
 
 export default function Community() {
@@ -19,13 +21,49 @@ export default function Community() {
     if (context.token) {
       getPosts()
     }
+    // console.log(context.token)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [context.token])
 
-  const post = () => {
+  const PostsList = () => {
     return posts.map((post) => {
+      let date = new Date(post.date)
+      const postPath = `/community/posts/${post._id}`
+      date = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear()
       return (
-        <></>
+        <Container key={post._id} style={{ marginBottom: '30px' }}>
+          <Row>
+            <Col className='flexRow'>
+              <Col sm={3} lg={2}>
+                <Image className='imgShadow' style={{ width: '72px' }} src={post.profile.avatar} roundedCircle />
+              </Col>
+              <Col >
+                <h4 style={{ marginBottom: 0, fontSize: '20px', fontWeight: '600' }}>{post.profile.name}</h4>
+                <p style={{ marginBottom: 0 }}>{post.profile.job_title}</p>
+              </Col>
+            </Col>
+            <Col style={{ textAlign: 'right' }}>
+              <p>{date}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={7} style={{ marginLeft: '30px', marginTop: '15px', fontWeight: 'bold', color: '#232B4E' }}>
+              <Row style={{ marginLeft: '5px' }}>
+                <Link style={{ color: '#232B4E', fontWeight: 'bold' }} to={postPath}>{post.title}</Link>
+              </Row>
+              <Row style={{ marginLeft: '5px',marginTop:'8px' }}>
+                <p style={{marginBottom:0}}><ChatSquareTextFill size={18} />  {post.comments.length}  <HeartFill style={{marginLeft:'5px'}} size={18}/>  {post.likes.length}</p>
+              </Row>
+            </Col>
+            {/* <Col style={{textAlign:'right',marginTop: '15px'}}>
+            Comments: {post.comments.length}
+            </Col>
+            <Col style={{textAlign:'right',marginTop: '15px'}}>
+            Likes: {post.likes.length}
+            </Col> */}
+          </Row>
+          <hr />
+        </Container >
       )
     })
   }
@@ -33,17 +71,17 @@ export default function Community() {
   return (
     <Container>
       <Row style={{ justifyContent: 'space-between' }}>
-        <Col style={{ borderLeft: 'solid', borderLeftColor: '#504EDF', borderLeftWidth: '3px', paddingLeft: '8px' }}>
-          <h2 >Discuss Your Problems With Our Community</h2>
+        <Col style={{ borderLeft: 'solid', height: '90%', borderRadius: '2px', borderLeftColor: '#504EDF', borderLeftWidth: '3px', paddingLeft: '8px' }}>
+          <h2 style={{ marginBottom: 0 }} >Discuss Your Problems With Our Community</h2>
         </Col>
-        <Col>
+        <Col style={{ textAlign: 'right', alignSelf: 'center' }}>
           <Button variant='outline-dark' className='buttonTopic' size="lg" type="submit" style={{ marginBottom: '50px', height: '40px', fontWeight: '500' }}>
-            New Topic
+            <PlusCircle style={{ paddingBottom: '2px' }} size={20} />  New Topic
           </Button>
         </Col>
       </Row>
-      <Row>
-
+      <Row style={{ marginTop: '40px' }}>
+        <PostsList />
       </Row>
     </Container>
   )
