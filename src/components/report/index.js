01659b577bb03f284d1460556fb050e2fc3c4494
Othabteh <o.thabteh@gmail.com>
2020-12-13 00:09:56 +0200

    working on the report utilites

--- src/components/report/index.js
+++ src/components/report/index.js
@@ -8,21 +8,22 @@ import { useHistory } from 'react-router-dom';
 
 export default function Report() {
   let history = useHistory();
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   async function getData() {
     const API = 'https://jobify-app-v2.herokuapp.com';
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudF90eXBlIjoicCIsInByb2ZpbGUiOnsiaWQiOjEsImZpcnN0IjoiTWFsZWsiLCJsYXN0IjoiQWhtZWQiLCJhdmF0YXIiOiJodHRwczovL2xpYnJhcnkua2lzc2NsaXBhcnQuY29tLzIwMTgwOTI5L29vcS9raXNzY2xpcGFydC1hdmF0YXItcGVyc29uLWNsaXBhcnQtYXZhdGFyLWNvbXB1dGVyLWljb25zLXBlcnNvbi04NzM1NWM1NmExNzQ4NDczLmpwZyIsImNvdW50cnkiOiJVU0EifSwiaWF0IjoxNjA3NjkzMzMxLCJleHAiOjM2MTYwNzY5MzMzMX0.UjxBO5cHePzHJaJcuZgQb-zZ1B_-XAFJYxfsKuUB_ig';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudF90eXBlIjoicCIsInByb2ZpbGUiOnsiaWQiOjEsImZpcnN0Ijoib3NhbWEiLCJsYXN0IjoiemF0YXIiLCJhdmF0YXIiOiJodHRwczovL2xpYnJhcnkua2lzc2NsaXBhcnQuY29tLzIwMTgwOTI5L29vcS9raXNzY2xpcGFydC1hdmF0YXItcGVyc29uLWNsaXBhcnQtYXZhdGFyLWNvbXB1dGVyLWljb25zLXBlcnNvbi04NzM1NWM1NmExNzQ4NDczLmpwZyIsImpvYl90aXRsZSI6IkRldmVsb3BlciIsImNvdW50cnkiOiJVU0EifSwiaWF0IjoxNjA3ODA4OTkyLCJleHAiOjM2MTYwNzgwODk5Mn0.N9ei6Q3kG4QNBXmDb4qGVDt0L_2HGk78VdJXTcjuvxY';
     const response = await superagent.get(`${API}/reports`).set('authorization', `Basic ${token}`);
-    console.log(response.body);
+    console.log('osamamamamamamammama', response.body);
     setData(response.body);
   }
   useEffect(() => {
     getData();
   }, []);
+
   return (
-    <Container sm={10} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-      <Row sm={6}>
+    <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '100%' }}>
+      <Col style={{ justifyContent: 'flex-start' }} sm={6}>
         {data.map((item) => {
           let state = 'Closed';
           let response = item.response;
@@ -31,20 +32,24 @@ export default function Report() {
             response = '';
           }
           return (
-            <Row style={{ display: 'flex', flexDirection: 'column' }}>
-              <Row style={{ display: 'flex', flexDirection: 'row' }}>
-                <Col>{item.id}</Col>
-                <Col>{state}</Col>
+            <Container style={{ display: 'flex', flexDirection: 'column', marginBottom: '15px', border: 'solid 1px red', padding: '0' }}>
+              <Row style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Col style={{ padding: '0', textAlign: 'center' }}>{item.id}</Col>
+                <Col style={{ padding: '0', textAlign: 'center' }}>{state}</Col>
               </Row>
-              <Row style={{ display: 'flex', flexDirection: 'column' }}>
-                <Col>{item.description}</Col>
-                <Col>{response}</Col>
+              <Row>
+                <Col style={{ padding: '0', textAlign: 'center' }}>{item.description}</Col>
               </Row>
-            </Row>
+              <Row>
+                <Col style={{ padding: '0', textAlign: 'center' }}>{response}</Col>
+              </Row>
+            </Container>
           );
         })}
-      </Row>
-      <Row sm={6}></Row>
+      </Col>
+      <Col>
+        <Row></Row>
+      </Col>
     </Container>
   );
 }
