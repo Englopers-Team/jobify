e534ab3232517add626bce70a2f470b82c7776c1
AbdulhakimZatar <hakimzatar@gmail.com>
2020-12-13 15:31:47 +0200

    fix chat

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -1,25 +1,30 @@
 import { useEffect, useState, useContext } from 'react';
 import { Badge, ListGroup, Form, Col, Container, Row, Image, Button } from 'react-bootstrap';
 import { SocketContext } from '../../context/socket';
+import { AuthContext } from '../../context/auth';
 
 export default function Notification() {
   const context = useContext(SocketContext);
+  const authContext = useContext(AuthContext);
 
   useEffect(() => {
-    if (context.token) {
+    console.log('here 0')
+
+    if (authContext.token) {
       console.log('here 1')
-      context.socketNotif.emit('join', context.token);
+      context.socketNotif.emit('join', authContext.token);
       context.socketNotif.on('notification', (payload) => {
         console.log(payload.auth_id, payload.title, payload.description);
       })
-      context.socketNotif.emit('checkNotif', { token: context.token })
+      context.socketNotif.emit('checkNotif', { token: authContext.token })
     }
 
     // return () => {
     //   context.socketNotif.off('notification');
     // };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context.token]);
+  }, [authContext.token]);
+
   return (
     <></>
   )
