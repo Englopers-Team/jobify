0212fcfd47c2f8a33fb72aac6cefc31231891bfc
Othabteh <o.thabteh@gmail.com>
2020-12-13 19:04:53 +0200

    working on the user reports

--- src/components/search/jobs/results.js
+++ src/components/search/jobs/results.js
@@ -1,10 +1,29 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
-import { If, Then } from 'react-if';
-
+import { useContext, useState, useEffect } from 'react';
+import { If, Then, Else } from 'react-if';
+import superagent from 'superagent';
 import Button from 'react-bootstrap/Button';
 export default function JobsResults(props) {
   let results = props.results;
+  const [show, setShow] = useState(false);
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudF90eXBlIjoicCIsInByb2ZpbGUiOnsiaWQiOjEsImZpcnN0IjoiTWFsZWsiLCJsYXN0IjoiQWhtZWQiLCJhdmF0YXIiOiJodHRwczovL2xpYnJhcnkua2lzc2NsaXBhcnQuY29tLzIwMTgwOTI5L29vcS9raXNzY2xpcGFydC1hdmF0YXItcGVyc29uLWNsaXBhcnQtYXZhdGFyLWNvbXB1dGVyLWljb25zLXBlcnNvbi04NzM1NWM1NmExNzQ4NDczLmpwZyIsImNvdW50cnkiOiJVU0EifSwiaWF0IjoxNjA3NjkzMzMxLCJleHAiOjM2MTYwNzY5MzMzMX0.UjxBO5cHePzHJaJcuZgQb-zZ1B_-XAFJYxfsKuUB_ig';
+  const [loader, setLoader] = useState(false);
+  const Apply = (payload) => {
+    const API = 'https://jobify-app-v2.herokuapp.com/user/apply';
+    setLoader(true);
+    superagent
+      .post(`${API}/${payload.job_id}`)
+      .set('authorization', `Basic ${token}`)
+      .send(payload)
+      .then((data) => {
+        console.log(data.text);
+
+        setLoader(false);
+        setShow(false);
+      });
+  };
   return (
     <>
       <If condition={props.visable}>
@@ -51,7 +70,33 @@ export default function JobsResults(props) {
                     </Button>
                   </Col>
                   <Col style={{ textAlign: 'center' }} className='button-col' sm={1}>
-                    <Button className='button' onClick={console.log(item.email)} variant='praimary'>
+                    <Button
+                      className='button'
+                      style={{ paddingRight: '50px', backgroundColor: '#504edf' }}
+                      onClick={() => {
+                        <If condition={item.job_id}>
+                          <Then>
+                            {Apply({
+                              job_id: item.job_id,
+                              company_id: item.company_id,
+                            })}
+                          </Then>
+                          <Else>
+                            {Apply({
+                              title: item.title,
+                              location: item.location,
+                              type: item.type,
+                              company_name: item.company_name,
+                              logo: item.logo,
+                              email: item.email,
+                              job_id: 0,
+                              api: true,
+                            })}
+                          </Else>
+                        </If>;
+                      }}
+                      variant='praimary'
+                    >
                       Apply
                     </Button>
                   </Col>
