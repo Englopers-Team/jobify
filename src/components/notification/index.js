93fb9f2cef3d6cd61b1f38251bbeb8190da1bd66
AbdulhakimZatar <hakimzatar@gmail.com>
2020-12-13 13:55:04 +0200

    chat

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -1,4 +1,24 @@
+import { useEffect, useState, useContext } from 'react';
+import { Badge, ListGroup, Form, Col, Container, Row, Image, Button } from 'react-bootstrap';
+import { SocketContext } from '../../context/socket';
+
 export default function Notification() {
+  const context = useContext(SocketContext);
+
+  useEffect(() => {
+    if (context.token) {
+      context.socketNotif.emit('join', context.token);
+      context.socketNotif.on('notification', (payload) => {
+        console.log(payload.auth_id, payload.title, payload.description);
+      })
+      context.socketNotif.emit('checkNotif', { token: context.token })
+    }
+
+    return () => {
+      context.socketNotif.off('notification');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context.token]);
   return (
     <></>
   )
--- src/context/socket.js
+++ src/context/socket.js
@@ -7,8 +7,10 @@ export const SocketContext = React.createContext();
 function SocketProvider(props) {
   const link = 'https://jobify-app-v2.herokuapp.com';
   const socketMessg = io(`${link}/messages`);
+  const socketNotif = io(`http://localhost:4000/notification`);
   const state = {
-    socketMessg
+    socketMessg,
+    socketNotif
   };
   return (
     <SocketContext.Provider value={state}>
