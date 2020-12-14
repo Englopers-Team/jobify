71d91dead5062410c4e4ef2130ce939bca7a75fe
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-12-14 15:02:09 +0200

    adminBlockstyle

--- src/components/admin/block.js
+++ src/components/admin/block.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import superagent from 'superagent';
 import { Container, Row, Col, Dropdown, FormControl, Image, FormCheck, FormLabel } from 'react-bootstrap';
 import { If, Then, Else } from 'react-if'
+import { MDBContainer } from "mdbreact";
+
+import './styles.scss';
 
 export default function Block() {
   const [person, setPerson] = useState([]);
@@ -10,6 +13,8 @@ export default function Block() {
   const [query, setQuery] = useState('');
   const [id, setId] = useState();
   const [searchTypeQuery, setSearchTypeQuery] = useState('Id');
+  const scrollContainerStyle = { backgroundColor: 'white', width: "auto", maxHeight: "500px", height: '500px', overflowY: 'scroll', overflowX: 'hidden' };
+
 
   const API = 'https://jobify-app-v2.herokuapp.com';
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiYWNjb3VudF90eXBlIjoiYWRtaW4iLCJwcm9maWxlIjp7fSwiaWF0IjoxNjA3NzA4MDY2LCJleHAiOjM2MTYwNzcwODA2Nn0.uErZAP_4ZCFUp-WLXIhXlV7SZu40itfj0C6m1Ppwm_c';
@@ -37,12 +42,12 @@ export default function Block() {
       return company.map((item, index) => {
         if (item.company_name.toLowerCase().includes(query.toLowerCase()) && searchTypeQuery === 'Username' || query == item.auth_id && searchTypeQuery === 'Id' || query === '') {
           return (
-            <Row key={index} onClick={() => { setId(item.auth_id) }}>
-              <Col>{item.auth_id}</Col>
-              <Col>
-                <Image src={`${item.avatar}`} roundedCircle style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
+            <Row className='profile' key={index} onClick={() => { setId(item.auth_id) }}>
+              <Col  sm={2} style={{ textAlign: 'center', lineHeight: '300%' , fontWeight : 'bold' , color : '#9393A1' }}>{item.auth_id}</Col>
+              <Col  sm={2}>
+                <Image src={`${item.logo}`} roundedCircle style={{ width: '38px', height: '38px', objectFit: 'cover' }} />
               </Col>
-              <Col>{item.company_name}</Col>
+              <Col  sm={8} style={{ margin: '0' , color : '#9393A1' }}>{item.company_name}</Col>
             </Row>
           )
         }
@@ -52,14 +57,14 @@ export default function Block() {
         let name = `${item.first_name} ${item.last_name}`
         if (name.toLowerCase().includes(query.toLowerCase()) && searchTypeQuery === 'Username' || query == item.auth_id && searchTypeQuery === 'Id' || query === '') {
           return (
-            <Row key={index} onClick={() => { setId(item.auth_id) }}>
-              <Col>{item.auth_id}</Col>
-              <Col>
-                <Image src={`${item.avatar}`} roundedCircle style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
+            <Row className='profile' key={index} onClick={() => { setId(item.auth_id) }}>
+              <Col  sm={2} style={{ textAlign: 'center', lineHeight: '300%' , fontWeight : 'bold' , color : '#9393A1' }} sm={2}>{item.auth_id}</Col>
+              <Col sm={2}>
+                <Image src={`${item.avatar}`} roundedCircle style={{ width: '38px', height: '38px', objectFit: 'cover' }} />
               </Col>
-              <Col>
-                <p>{name}</p>
-                <p>{item.job_title}</p>
+              <Col sm={8}>
+                <p style={{ margin: '0' , color : '#9393A1' }}>{name}</p>
+                <p style={{ margin: '0'  , color : '#9393A1'}}>{item.job_title}</p>
               </Col>
             </Row>
           )
@@ -82,7 +87,7 @@ export default function Block() {
       data = (
         <Row>
           <Col>{target.auth_id}</Col>
-          <Image src={`${target.avatar}`} roundedCircle style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
+          <Image src={`${target.avatar}`} roundedCircle style={{ width: '38px', height: '38px', objectFit: 'cover' }} />
           <Col>{target.company_name}</Col>
         </Row>
       )
@@ -90,9 +95,11 @@ export default function Block() {
       let name = `${target.first_name} ${target.last_name}`
       data = (
         <Row>
-          <Col>{target.auth_id}</Col>
-          <Image src={`${target.avatar}`} roundedCircle style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
-          <Col>
+          <Col style={{  color : '#9393A1'}} sm={2}>{target.auth_id}</Col>
+          <Col sm={4}>
+            <Image src={`${target.avatar}`} roundedCircle style={{ width: '38px', height: '38px', objectFit: 'cover' }} />
+          </Col>
+          <Col sm={6}>
             <p>{name}</p>
             <p>{target.job_title}</p>
           </Col>
@@ -104,7 +111,7 @@ export default function Block() {
   return (
     <Container>
       <Row sm={10}>
-        <Col sm={8}>
+        <Col sm={7}>
           <Row>
             <FormCheck type="switch" name="formHorizontalSwitch" id="custom" label="Search By Username" onChange={(e) => { setSearchTypeQuery(searchTypeQuery === 'Id' ? 'Username' : 'Id') }} />
             <FormControl placeholder={`Search By ${searchTypeQuery}`} onChange={(e) => { setQuery(e.target.value) }} />
@@ -113,10 +120,14 @@ export default function Block() {
           </Row>
           <Row>
             <Result />
+
           </Row>
         </Col>
-        <Col sm={4}>
-          <List />
+        <Col sm={5}>
+          <MDBContainer className="scrollbar scrollbar-primary" style={scrollContainerStyle}>
+            <List />
+          </MDBContainer>
+
         </Col>
       </Row>
     </Container>
