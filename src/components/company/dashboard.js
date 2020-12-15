07e0288686c448d5f19dc3f6ae16b1180ec86146
messeili <mymoa.2014@gmail.com>
2020-12-15 18:54:20 +0200

    finish user dashoard

--- src/components/company/dashboard.js
+++ src/components/company/dashboard.js
@@ -17,7 +17,7 @@ export default function CompanyDashboard() {
   const [companyName, setCompanyName] = useState('');
   const [logo, setLogo] = useState('');
   const [notification, setNotification] = useState([]);
-  const [loader, setLoader] = useState(true);
+  const [location, setLocation] = useState(true);
   const [screenSize, setScreenSize] = useState(window.innerWidth);
   const [applications, setApplications] = useState([[], '']);
   const [jobsData, setJobs] = useState([[], '']);
@@ -54,14 +54,10 @@ export default function CompanyDashboard() {
   }, [context.token]);
 
   async function getData() {
-    const flagData = await superagent.get(`${API}/flag`);
-    setFlag(flagData.body);
     const response = await superagent.get(`${API}/getInfo`).set('authorization', `Basic ${context.token}`);
     setCompanyName(response.body.company_name);
     setLogo(response.body.logo);
-    const data = await superagent.get(`${API}/home`).set('authorization', `Basic ${context.token}`);
-    setNotification(data.body.notifications);
-    data.body.notifications[0] ? setSeen(data.body.notifications[data.body.notifications.length - 1].seen) : setSeen(true);
+    setLocation(response.body.country);
   }
 
   const appList = async (e) => {
@@ -99,7 +95,9 @@ export default function CompanyDashboard() {
       <Container className='content' style={{ margin: '70px auto' }}>
         <Row style={{ justifyContent: 'space-between' }}>
           <Col style={{ borderLeft: 'solid', height: '90%', borderRadius: '2px', borderLeftColor: '#504EDF', borderLeftWidth: '3px', paddingLeft: '8px', marginBottom: 30 }}>
-            <h2 style={{ marginBottom: 0 }}>Employer Dashboard</h2>
+            <h2 style={{ color: '#504edf', fontSize: '45px', fontWeight: 'bold' }}>
+              Employer <span style={{ color: '#515151', fontSize: '45px', fontWeight: 'bold' }}>Dashboard</span>
+            </h2>
           </Col>
         </Row>
         <Row>
@@ -109,17 +107,17 @@ export default function CompanyDashboard() {
             </CardTitle>
             <Card className='card-user'>
               <div className='image'>
-                <img alt='...' style={{ width: '100%' }} src='https://www.kindpng.com/picc/m/150-1500467_interview-clipart-transparent-picture-library-stock-interview-png.png' />
+                <img alt='...' style={{ width: '100%', borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }} src='https://marketplace.canva.com/EAD7TRce0QM/1/0/800w/canva-corporate-professional-linkedin-banner-XkzsOmdpVyk.jpg' />
               </div>
               <CardBody>
                 <div className='author'>
                   <img alt='...' className='avatar border-gray' src={logo} />
-                  <h5 style={{ fontWeight: 600 }} className='title'>
+                  <h5 style={{ fontWeight: 600, fontSize: 30 }} className='title'>
                     {companyName}
                   </h5>
                   {/* <p className='description'>@chetfaker</p> */}
                 </div>
-                <p className='description text-center'>Amman</p>
+                <p className='description text-center'>{location}</p>
               </CardBody>
               <CardFooter>
                 <hr />
@@ -205,7 +203,7 @@ export default function CompanyDashboard() {
               <CardBody>
                 {applications[0].map((item) => {
                   return (
-                    <Row className='flexRow list-body' sm={12} style={{ justifyContent: screenSize > 1199 ? 'space-between' : 'center' }}>
+                    <Row className='flexRow list-body-one' sm={12} style={{ justifyContent: screenSize > 1199 ? 'space-between' : 'center' }}>
                       <Col style={{ fontWeight: 650, textAlign: screenSize > 575 ? 'left' : 'center' }} sm={2} lg={2}>
                         <Image src={item.avatar} roundedCircle style={{ width: 50, height: 50, objectFit: 'cover' }} />
                       </Col>
@@ -241,7 +239,7 @@ export default function CompanyDashboard() {
                   <Col style={{ textAlign: screenSize > 575 ? 'left' : 'center', color: '#515151' }} sm={2} lg={2}>
                     <p>Type</p>
                   </Col>
-                  <Col style={{ textAlign: 'left', color: '#515151' }} sm={3} lg={2}>
+                  <Col style={{ textAlign: screenSize > 575 ? 'left' : 'center', color: '#515151' }} sm={3} lg={2}>
                     <p>Location</p>
                   </Col>
                   <Col style={{ textAlign: 'center', color: '#515151' }} sm={2} lg={2}>
@@ -275,6 +273,9 @@ export default function CompanyDashboard() {
               </CardBody>
             </Card>
           </Col>
+        </Row>
+        <Row style={{ justifyContent: 'center', margin: 20 }}>
+          <Image style={{ width: '70%' }} src='../../assets/undraw_Work_time_re_hdyv.svg'></Image>
         </Row>
       </Container>
     </>
