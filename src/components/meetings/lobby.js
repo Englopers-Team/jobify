517dcfdc17b18eff44c31b22fced6be54467aab3
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-12-31 18:58:02 +0200

    addDepend

--- src/components/meetings/lobby.js
+++ src/components/meetings/lobby.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import Calendar from 'react-calendar'
+import { If, Then, Else } from 'react-if'
+import io from "socket.io-client";
+
 import stream from './stream';
 import meetings from './meetings';
 import profile from './profile';
