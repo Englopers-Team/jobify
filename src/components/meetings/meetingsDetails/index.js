eafe53ee85fc37643da4c84e609cd2af7accf8ab
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-12-31 20:23:35 +0200

    handleScreenShows

--- src/components/meetings/meetingsDetails/index.js
+++ src/components/meetings/meetingsDetails/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import {  Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 
 function Meetings(props) {
-  
+  return (
+    <>
+      <Row >
+        {Object.keys(props.users).map(id => {
+          if (id === props.yourID) {
+            return null;
+          }
+          return (
+            <button key={id} onClick={() => {
+              props.setUserToCall(id)
+              props.setShow(true)
+            }}>Call {id}</button>
+          );
+        })}
+      </Row>
+    </>
+  )
 }
 export default Meetings;
