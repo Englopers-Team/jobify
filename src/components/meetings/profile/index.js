2a20ec3987d1fe133a77f23da152c0970dc0383d
AbdulhakimZatar <hakimzatar@gmail.com>
2021-01-03 21:14:29 +0200

    meeting company profile

--- src/components/meetings/profile/index.js
+++ src/components/meetings/profile/index.js
@@ -15,11 +15,11 @@ function Profile(props) {
   const [btn, setBtn] = useState('Applicant  Resume')
 
   useEffect(() => {
-    if (pdf) {
+    if (pdf && userInfoMain.avatar) {
       const frame = document.getElementById('iframepdf')
       frame.style.height = '95vh'
       setBtn('Applicant Profile')
-    } else {
+    } else if (userInfoMain.avatar) {
       const frame = document.getElementById('iframepdf')
       frame.style.height = '0'
       setBtn('Applicant  Resume')
@@ -46,40 +46,74 @@ function Profile(props) {
   }, [context.token, props.authIdToCall])
   return (
     <Container style={{ height: '100%' }}>
-      <Row style={{ justifyContent: 'center' }}>
-        <img style={{ width: '128px', height: '128px', objectFit: 'cover', borderRadius: '50%' }} src={userInfoMain.avatar ? userInfoMain.avatar : userInfoMain.logo}></img>
-      </Row>
-      <Row class='flexCol' style={{ dispaly: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '20px' }}>
-        <p style={{ fontSize: '24px', marginBottom: '0', fontWeight: 'bold', textAlign: 'center' }}>{userInfoMain.first_name} {userInfoMain.last_name}</p>
-        <p style={{ fontSize: '16px', fontWeight: '500', textAlign: 'center' }}>{userInfoMain.job_title}</p>
-      </Row>
-      <hr style={{ width: '70%', margin: '0 auto' }}></hr>
+      <If condition={userInfoMain.avatar}>
+        <Then>
+          <Row style={{ justifyContent: 'center' }}>
+            <img style={{ width: '128px', height: '128px', objectFit: 'cover', borderRadius: '50%' }} src={userInfoMain.avatar ? userInfoMain.avatar : userInfoMain.logo}></img>
+          </Row>
+          <Row class='flexCol' style={{ dispaly: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '20px' }}>
+            <p style={{ fontSize: '24px', marginBottom: '0', fontWeight: 'bold', textAlign: 'center' }}>{userInfoMain.first_name} {userInfoMain.last_name}</p>
+            <p style={{ fontSize: '16px', fontWeight: '500', textAlign: 'center' }}>{userInfoMain.job_title}</p>
+          </Row>
+          <hr style={{ width: '70%', margin: '0 auto' }}></hr>
 
-      <Row style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px', marginLeft: '20px', marginRight: '20px' }}>
-        <Col style={{ textAlign: 'center' }}>
-          <p style={{ fontSize: '16px', textAlign: 'left' }}><strong style={{ marginRight: '5px' }}>Location:</strong> {userInfoMain.country}</p>
-        </Col>
-        <Col style={{ textAlign: 'center' }}>
-          <p style={{ fontSize: '16px', textAlign: 'right' }}><strong style={{ marginRight: '5px' }}>Age:</strong> {userInfoMain.age}</p>
+          <Row style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px', marginLeft: '20px', marginRight: '20px' }}>
+            <Col style={{ textAlign: 'center' }}>
+              <p style={{ fontSize: '16px', textAlign: 'left' }}><strong style={{ marginRight: '5px' }}>Location:</strong> {userInfoMain.country}</p>
+            </Col>
+            <Col style={{ textAlign: 'center' }}>
+              <p style={{ fontSize: '16px', textAlign: 'right' }}><strong style={{ marginRight: '5px' }}>Age:</strong> {userInfoMain.age}</p>
 
-        </Col>
+            </Col>
 
-      </Row>
-      <Row style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px', marginLeft: '20px', marginRight: '20px' }}>
-        <Col style={{ textAlign: 'center' }}>
-          <p style={{ fontSize: '18px', fontWeight: 'bold', textAlign: 'center' }}>Summery</p>
-          <p style={{ fontSize: '16px', textAlign: 'left' }}>{userInfoMain.sammary}</p>
-        </Col>
-      </Row>
+          </Row>
+          <Row style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px', marginLeft: '20px', marginRight: '20px' }}>
+            <Col style={{ textAlign: 'center' }}>
+              <p style={{ fontSize: '18px', fontWeight: 'bold', textAlign: 'center' }}>Summery</p>
+              <p style={{ fontSize: '16px', textAlign: 'left' }}>{userInfoMain.sammary}</p>
+            </Col>
+          </Row>
+          <iframe style={{ height: '0%', position: 'absolute', bottom: '5vh', top: 0, border: 0, left: 0, width: '100%', overflow: 'scroll' }} id="iframepdf" src={userInfoMain.cv}></iframe>
+
+          <button style={{ background: '#504edf', fontSize: '18px', height: '5vh', fontWeight: 'bold', position: 'absolute', bottom: 0, border: 0, left: 0, width: '100%' }} onClick={() => {
+
+            setPDF(!pdf)
+            // window.open(userInfoMain.cv, 'popUpWindow', 'height=800,width=600,left=10,top=10,,scrollbars=yes,menubar=no'); return false;
+          }}>{btn}</button>
+        </Then>
+        <Else>
+          <If condition={userInfoMain.logo}>
+            <Then>
+              <Row style={{ justifyContent: 'center' }}>
+                <img style={{ width: '128px', height: '128px', objectFit: 'cover', borderRadius: '50%' }} src={userInfoMain.avatar ? userInfoMain.avatar : userInfoMain.logo}></img>
+              </Row>
+              <Row class='flexCol' style={{ dispaly: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '20px' }}>
+                <p style={{ fontSize: '24px', marginBottom: '0', fontWeight: 'bold', textAlign: 'center' }}>{userInfoMain.company_name}</p>
+              </Row>
+              <hr style={{ width: '70%', margin: '0 auto' }}></hr>
+
+              <Row style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px', marginLeft: '20px', marginRight: '20px' }}>
+                <Col style={{ textAlign: 'center' }}>
+                  <p style={{ fontSize: '16px', textAlign: 'left' }}><strong style={{ marginRight: '5px' }}>Location:</strong> {userInfoMain.country}</p>
+                </Col>
+                <Col style={{ textAlign: 'center' }}>
+                  <p style={{ fontSize: '16px', textAlign: 'right' }}><strong style={{ marginRight: '5px' }}>Phone:</strong> {userInfoMain.phone}</p>
+
+                </Col>
+
+              </Row>
+
+              <button style={{ background: '#504edf', fontSize: '18px', height: '5vh', fontWeight: 'bold', position: 'absolute', bottom: 0, border: 0, left: 0, width: '100%' }} onClick={() => {
 
 
-      <iframe style={{ height: '0%', position: 'absolute', bottom: '5vh', top: 0, border: 0, left: 0, width: '100%', overflow: 'scroll' }} id="iframepdf" src={userInfoMain.cv}></iframe>
+                window.open(`http://${userInfoMain.company_url}`, 'popUpWindow', 'height=800,width=600,left=10,top=10,,scrollbars=yes,menubar=no'); return false;
+              }}>Company website</button>
+            </Then>
+          </If>
+        </Else>
+      </If>
 
-      <button style={{ background: '#504edf', fontSize: '18px', height: '5vh', fontWeight: 'bold', position: 'absolute', bottom: 0, border: 0, left: 0, width: '100%' }} onClick={() => {
 
-        setPDF(!pdf)
-        // window.open(userInfoMain.cv, 'popUpWindow', 'height=800,width=600,left=10,top=10,,scrollbars=yes,menubar=no'); return false;
-      }}>{btn}</button>
       {console.log('userInfoSecondary', userInfoSecondary)}
       {console.log('userInfoMain', userInfoMain)}
     </Container>
