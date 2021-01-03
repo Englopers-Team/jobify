2f082b88f97190d4cea710cf503eeede94f9cafc
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2021-01-03 19:02:54 +0200

    fixed

--- src/components/meetings/profile/index.js
+++ src/components/meetings/profile/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef , useContext } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import superagent from 'superagent';
 import { AuthContext } from '../../../context/auth';
@@ -6,23 +6,33 @@ import { AuthContext } from '../../../context/auth';
 
 
 function Profile(props) {
-  const [userInfoSecondary ,setUserInfo] = useState([])
+  const [userInfoSecondary, setUserInfoSecondary] = useState([])
+  const [userInfoMain, setUserInfoMain] = useState([])
+
   const context = useContext(AuthContext);
 
   async function getSecondaryData() {
     const API = 'https://jobify-app-v2.herokuapp.com';
     const response = await superagent.get(`${API}/search/get-full-info/${props.authIdToCall}`).set('authorization', `Basic ${context.token}`);
-    setUserInfo(response.body);
+    setUserInfoSecondary(response.body);
   }
 
-  useEffect(()=>{
+  async function getmainData() {
+    const API = 'https://jobify-app-v2.herokuapp.com';
+    const response = await superagent.get(`${API}/getinfo/${props.authIdToCall}`).set('authorization', `Basic ${context.token}`);
+    setUserInfoMain(response.body);
+  }
+
+  useEffect(() => {
     getSecondaryData()
-  },[context.token,props.authIdToCall])
+    getmainData()
+  }, [context.token, props.authIdToCall])
   return (
     <>
       <h2>Profile</h2>
       <p>{props.authIdToCall}</p>
-      {console.log('userInfoSecondary',userInfoSecondary)}
+      {console.log('userInfoSecondary', userInfoSecondary)}
+      {console.log('userInfoMain', userInfoMain)}
     </>
   )
 }
